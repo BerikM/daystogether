5ca66115b7dbe8e5ca29f808c7d65ff1a74369e7
BerikM <109854504+BerikM@users.noreply.github.com>
2022-11-04 00:30:52 +0600

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,20 @@ const diffInDays = Math.floor(diffTime / oneDay);
 const hours =  Math.floor((diffTime / oneHours)% 24)
 
 let dateText = document.getElementsByClassName("day-text");
-dateText[0].innerText = `Времени вместе: ${diffInDays} дней ${hours} часов`
+dateText[0].innerText = `Времени вместе: ${diffInDays} день ${hours} час`;
+
+
+const firstKiss = Date.UTC(2022,8,14,0,0,0)
+const firstSex = Date.UTC(2022,8,17,0,0,0)
+const relation = Date.UTC(2022,9,16,0,0,0)
+const kissAfterDay =  Math.floor((dateNow - firstKiss) / oneDay);
+const sexAfterDay =  Math.floor((dateNow - firstSex) / oneDay);
+const relationAfterDay =  Math.floor((dateNow - relation) / oneDay);
+
+const kissAfterHours =  Math.floor(((dateNow - firstKiss) / hours)%24);
+const sexAfterHours =  Math.floor(((dateNow - firstSex) / hours)%24);
+const relationAfterHours =  Math.floor(((dateNow - relation) / hours)%24);
+
+document.getElementsByClassName("kiss-after")[0].innerText = `Прошло ${kissAfterDay} день и ${kissAfterHours} час`;
+document.getElementsByClassName("sex-after")[0].innerText = `Прошло ${sexAfterDay} день и ${sexAfterHours} час`;
+document.getElementsByClassName("relation-after")[0].innerText = `Прошло ${relationAfterDay} день и ${sexAfterDay} час`;
